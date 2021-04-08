6afe9761c3dd88072e89f33011f975f9066b73a2
Wil Stead <wil.stead@gmail.com>
2021-04-08 15:58:52 -0400

    Initial commit

---
+++ src/Extensions.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Azure.Cosmos;
+using System.Threading.Tasks;
+
+namespace Tavenem.DataStorage.Cosmos
+{
+    /// <summary>
+    /// Extension methods.
+    /// </summary>
+    public static class DataStorageCosmosExtensions
+    {
+        /// <summary>
+        /// Returns a <see cref="CosmosPagedList{T}"/> wrapper for the current <see
+        /// cref="FeedIterator{T}"/>.
+        /// </summary>
+        /// <typeparam name="T">The type of items in the collection.</typeparam>
+        /// <param name="iterator">The current <see cref="FeedIterator{T}"/>.</param>
+        /// <param name="pageNumber">The current page number.</param>
+        /// <param name="pageSize">The page size.</param>
+        /// <returns>A <see cref="CosmosPagedList{T}"/> containing the items in the current
+        /// collection.</returns>
+        public static Task<CosmosPagedList<T>> AsCosmosPagedListAsync<T>(
+            this FeedIterator<T>? iterator,
+            long pageNumber,
+            long pageSize)
+            => CosmosPagedList<T>.FromFeedIteratorAsync(iterator, pageNumber, pageSize);
+    }
+}
